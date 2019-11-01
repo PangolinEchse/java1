a2cc6f4577040053f00eebf0c62dd1b18261ed1c
PangolinEchse <55272412+PangolinEchse@users.noreply.github.com>
2019-11-01 10:34:44 +0200

    Add files via upload
    laba 4

---
+++ Lab_4.java
@@ -0,0 +1,23 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Lab_4 {
+    public static void main(String[] args) {
+
+        try{
+            CReader c = new CReader();
+            c.delComments();
+            c.printText();
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("FNF");
+        }
+        catch (IOException e) {
+            System.out.println("IOE");
+        }
+        catch(Exception e){
+            System.out.println("File error");
+        }
+
+    }
+}
